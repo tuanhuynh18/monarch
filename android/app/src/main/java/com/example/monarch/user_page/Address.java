0997dc43e27e4ef4a207a72775ea4dd2b2c0a53d
Yuxin Xie <dawndawn@yuxins-mbp.lan>
2021-10-20 11:55:48 -0400

    add GPS locate button add logout button

---
+++ android/app/src/main/java/com/example/monarch/user_page/Address.java
@@ -0,0 +1,58 @@
+package com.example.usr_page_test.user_page;
+
+public class Address {
+    String address;// = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+    String city; // = addresses.get(0).getLocality();
+    String state; //= addresses.get(0).getAdminArea();
+    String country; // = addresses.get(0).getCountryName();
+    String postalCode; //  = addresses.get(0).getPostalCode();
+    String knownName;   //= addresses.get(0).getFeatureName(); // Only if availab
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getKnownName() {
+        return knownName;
+    }
+
+    public void setKnownName(String knownName) {
+        this.knownName = knownName;
+    }
+}
---
+++ android/app/src/main/java/com/example/monarch/user_page/MyLocation.java
@@ -0,0 +1,166 @@
+package com.example.usr_page_test.user_page;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.permissionx.guolindev.PermissionX;
+
+public class MyLocation {
+    Timer timer1;
+    LocationManager lm;
+    LocationResult locationResult;
+    boolean gps_enabled = false;
+    boolean network_enabled = false;
+
+    public boolean getLocation(Context context, LocationResult result) {
+        //I use LocationResult callback class to pass location value from MyLocation to user code.
+        locationResult = result;
+        //PermissionX.init(this)    .permissions(Manifest.permission.CAMERA, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.RECORD_AUDIO)    .onExplainRequestReason { scope, deniedList ->val message = "PermissionX需要您同意以下权限才能正常使用"        scope.showRequestReasonDialog(deniedList, message, "确定", "取消")    }    .request { allGranted, grantedList, deniedList ->if (allGranted) {            Toast.makeText(activity, "所有申请的权限都已通过", Toast.LENGTH_SHORT).show()        } else {            Toast.makeText(activity, "您拒绝了如下权限：$deniedList", Toast.LENGTH_SHORT).show()        }    }
+
+
+        if (lm == null)
+            lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        //exceptions will be thrown if provider is not permitted.
+        try {
+            gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        } catch (Exception ex) {
+        }
+        try {
+            network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception ex) {
+        }
+
+        //don't start listeners if no provider is enabled
+        if (!gps_enabled && !network_enabled)
+            return false;
+
+        if (gps_enabled)
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                Log.e("error", "gps error");
+            }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListenerGps);
+        if (network_enabled)
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                Log.e("error", "gps error");
+            }
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListenerNetwork);
+        timer1 = new Timer();
+        timer1.schedule(new GetLastLocation(), 20000);
+        return true;
+    }
+
+    public void basePermissionX(Activity activity){
+        PermissionX.init((FragmentActivity) activity)
+                .permissions(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                .request((allGranted, grantedList, deniedList) -> {
+                    if (allGranted) {
+                        Toast.makeText(activity,"所有申请的权限都已通过", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(activity, "您拒绝了如下权限："+deniedList, Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
+
+    LocationListener locationListenerGps = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            timer1.cancel();
+            locationResult.gotLocation(location);
+            lm.removeUpdates(this);
+            lm.removeUpdates(locationListenerNetwork);
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    };
+
+    LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            timer1.cancel();
+            locationResult.gotLocation(location);
+            lm.removeUpdates(this);
+            lm.removeUpdates(locationListenerGps);
+        }
+
+        public void onProviderDisabled(String provider) {
+        }
+
+        public void onProviderEnabled(String provider) {
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+    };
+
+    class GetLastLocation extends TimerTask {
+        @SuppressLint("MissingPermission")
+        @Override
+        public void run() {
+            lm.removeUpdates(locationListenerGps);
+            lm.removeUpdates(locationListenerNetwork);
+            Location net_loc = null, gps_loc = null;
+            if (gps_enabled)
+                gps_loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(network_enabled)
+                net_loc=lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            //if there are both values use the latest one
+            if(gps_loc!=null && net_loc!=null){
+                if(gps_loc.getTime()>net_loc.getTime())
+                    locationResult.gotLocation(gps_loc);
+                else
+                    locationResult.gotLocation(net_loc);
+                return;
+            }
+
+            if(gps_loc!=null){
+                locationResult.gotLocation(gps_loc);
+                return;
+            }
+            if(net_loc!=null){
+                locationResult.gotLocation(net_loc);
+                return;
+            }
+            locationResult.gotLocation(null);
+        }
+    }
+
+    public static abstract class LocationResult{
+        public abstract void gotLocation(Location location);
+    }
+}
