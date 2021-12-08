65d24637b2ef70960516cc30afb5130e7b45086e
Tuan Huynh <thuynh71@gatech.edu>
2021-12-08 18:11:38 -0500

    register activity connect with backend

--- android/app/src/main/java/com/example/monarch/RegisterAccountActivity.java
+++ android/app/src/main/java/com/example/monarch/RegisterAccountActivity.java
@@ -2,13 +2,80 @@ package com.example.monarch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.monarch.API.RequestQueueSingleton;
+import com.example.monarch.data.User;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class RegisterAccountActivity extends AppCompatActivity {
+    private static final String TAG = "Register";
+
+    private Button mRegisterBtn;
+    private EditText mUsername;
+    private EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
+
+        // wire widget
+        mUsername = findViewById(R.id.register_email_edit_text);
+        mPassword = findViewById(R.id.register_password_edit_text);
+        mRegisterBtn = findViewById(R.id.register_button);
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = getResources().getString(R.string.back_end_base) + getResources().getString(R.string.register_endpoint);
+                JSONObject registerData = new JSONObject();
+                JSONObject body = new JSONObject();
+                try {
+                    registerData.put("email", mUsername.getText());
+                    registerData.put("password", mPassword.getText());
+                    body.put("user", registerData);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                JsonObjectRequest loginRequest = new JsonObjectRequest
+                        (Request.Method.POST, url, body, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d(TAG, "Register successfully");
+                                mPassword.setText("");
+                                mUsername.setText("");
+                                mPassword.clearFocus();
+                                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                                imm.hideSoftInputFromWindow(mRegisterBtn.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                                Toast toast = Toast.makeText(getApplicationContext(), "Register successfully", Toast.LENGTH_LONG);
+                                toast.show();
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        });
+                RequestQueueSingleton.getInstance(getApplicationContext()).addToRequestQueue(loginRequest);
+                Log.d(TAG, "Made login request" + body.toString());
+            }
+        });
     }
 }
